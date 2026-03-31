a69396807359911202a7e71338a1aa1a5adeb910
Sergio Rod <vsergiorodriguez@gmail.com>
2026-03-30 20:42:34 -0500

    Test01Error

--- src/main/java/cibertec/com/airline/reservation/FlightReservation.java
+++ src/main/java/cibertec/com/airline/reservation/FlightReservation.java
@@ -8,7 +8,7 @@ public class FlightReservation {
             String code, String passengerName,
             String passportNumber,int seats, LocalDate flightDate)
     {
-    return "La reserva ha sido registrada correctamente";
+    return "null";
     }
 
 }
