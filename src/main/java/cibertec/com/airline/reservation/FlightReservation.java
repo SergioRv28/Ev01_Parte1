03cf1d8e8b3e99ca60ef4b365c2fbb11285c8032
Sergio Rod <vsergiorodriguez@gmail.com>
2026-03-30 20:11:37 -0500

    Test01Error

---
+++ src/main/java/cibertec/com/airline/reservation/FlightReservation.java
@@ -0,0 +1,14 @@
+package cibertec.com.airline.reservation;
+
+
+import java.time.LocalDate;
+
+public class FlightReservation {
+    public String registerReservation(
+            String code, String passengerName,
+            String passportNumber,int seats, LocalDate flightDate)
+    {
+    return null;
+    }
+
+}
---
+++ src/test/java/cibertec/com/airline/reservation/FlightReservationTest.java
@@ -0,0 +1,26 @@
+package cibertec.com.airline.reservation;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlightReservationTest {
+
+    @Test
+    @DisplayName("Caso 1")
+    void testValidReservation(){
+        FlightReservation reservation = new FlightReservation();
+        String code="ABC123";
+        String passengerName= "Sergio Rodriguez";
+        String passportNumber= "AB1234567";
+        int seats = 2;
+        LocalDate flightDate = LocalDate.of(2026,12,31);
+
+        String result = reservation.registerReservation(code, passengerName, passportNumber, seats, flightDate);
+
+        assertEquals("La reserva ha sido registrada correctamente", result);
+    }
+}
