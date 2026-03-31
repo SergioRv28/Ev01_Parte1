769a96cf79cba67ecf42c130c44d4afc2c294dc5
Sergio Rod <vsergiorodriguez@gmail.com>
2026-03-30 21:05:16 -0500

    Test02Exito

--- src/main/java/cibertec/com/airline/reservation/FlightReservation.java
+++ src/main/java/cibertec/com/airline/reservation/FlightReservation.java
@@ -6,8 +6,13 @@ import java.time.LocalDate;
 public class FlightReservation {
     public String registerReservation(
             String code, String passengerName,
-            String passportNumber,int seats, LocalDate flightDate)
-    {
+            String passportNumber,int seats, LocalDate flightDate){
+
+        // validamos el codigo de reserva
+        if( code== null || !code.matches("[A-Z]{3}[0-9]{3}")){
+            return "Ingrese un codigo de reserva valido";
+        }
+
     return "La reserva ha sido registrada correctamente";
     }
 
